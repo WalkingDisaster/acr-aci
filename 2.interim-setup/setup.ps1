9e705faf4f0b89ac92c73b2c813c0e0a0bec72cf
Michael Meadows <michaelmeadows@gmail.com>
2023-09-15 00:11:27 -0400

    done but with improvements and fixed too

--- 2.interim-setup/setup.ps1
+++ 2.interim-setup/setup.ps1
@@ -18,13 +18,8 @@ docker pull nginx
 docker tag nginx "$AcrName.azurecr.io/samples/nginx"
 docker push "$AcrName.azurecr.io/samples/nginx"
 
-#$assignee = (az identity show -g $rg -n $aciIdentity --query "clientId" | ConvertFrom-Json)
-
-# Get resource ID of the user-assigned identity
-userID = $(az identity show --resource-group $ResourceGroup --name $AciIdentity --query id --output tsv)
-
 # Get service principal ID of the user-assigned identity
-spId = $(az identity show --resource-group $ResourceGroup --name $AciIdentity --query principalId --output tsv)
+$spId = $(az identity show --resource-group $ResourceGroup --name $AciIdentity --query principalId --output tsv)
 
 $acrId = (az resource show -g $rg -n $acrName --resource-type "Microsoft.ContainerRegistry/registries" --query "id")
 az role assignment create --assignee $spId --scope $acrId --role AcrPull
